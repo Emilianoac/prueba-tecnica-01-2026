d9ade4f67dae288837cf709f0d9b745b9b12e7ba
Emiliano <47641194+Emilianoac@users.noreply.github.com>
2026-01-29 12:32:38 -0300

    chore(config): ajustar base url de Vite con el nombre del repositorio de github

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/prueba-tecnica-01-2026',
   plugins: [
     vue(),
     vueDevTools(),
