21194978f6256d0abc1dfbc269f90d41010a5021
Emiliano <47641194+Emilianoac@users.noreply.github.com>
2026-01-29 00:23:34 -0300

    feat: añadir store para manejo de articulos

---
+++ src/stores/articles.store.ts
@@ -0,0 +1,63 @@
+import { defineStore } from "pinia";
+import { articleService } from "@/services/api/article/articleService";
+import type { Article } from "@/types/article/article.type";
+
+export const useArticlesStore = defineStore("articles", {
+  state: () => ({
+    articles: [] as Article[],
+    selectedArticle: null as Article | null,
+    search: "",
+
+    loading: false,
+    error: null as string | null
+  }),
+
+  getters: {
+    filteredArticles: (state) => {
+      const search = state.search.trim().toLowerCase();
+      
+      if (!search) return state.articles;
+      
+      return state.articles.filter(article =>
+        article.title.toLowerCase().includes(search)
+      );
+    },
+
+    numberOfArticles(): number {
+      return this.filteredArticles.length;
+    }
+  },
+
+  actions: {
+    async fetchArticles() {
+      if (this.articles.length) return;
+
+      this.loading = true;
+      this.error = null;
+
+      try {
+        this.articles = await articleService.getArticles(10);
+      } catch {
+        this.error = "Error cargando artículos";
+      } finally {
+        this.loading = false;
+      }
+    },
+
+    setSearch(value: string) {
+      this.search = value;
+    },
+
+    resetSearch() {
+      this.search = "";
+    },
+
+    selectArticle(article: Article) {
+      this.selectedArticle = article;
+    },
+
+    clearSelectedArticle() {
+      this.selectedArticle = null;
+    }
+  }
+});
